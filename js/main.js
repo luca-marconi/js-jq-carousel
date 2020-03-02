72c18fce661b5f000bc9dc9b3f91de2bd497ce29
Luca <luca-marconi@hotmail.com>
2020-03-02 18:06:39 +0100

    animazione freccia prev

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $('.next').click(function() { // oppure anche cosi si puo scrivere  $('.next').click(nextImage);
         nextSlide();
     });
+    $('.prev').click(function() { // oppure anche cosi si puo scrivere  $('.next').click(nextImage);
+        prevSlide();
+    });
 
     function nextSlide() {
         if ($('.images img.active').hasClass('last')) {
@@ -27,15 +30,38 @@ $(document).ready(function() {
             prossimaPallino.addClass('active');
         }
     }
+    function prevSlide() {
+        if ($('.images img.active').hasClass('first')) {
+
+            $('.images img.active').removeClass('active');
+            $('.images img.last').addClass('active');
+
+            $('.slider-nav i.active').removeClass('active');
+            $('.slider-nav i.last').addClass('active');
+
+        } else {
+            var imgAttiva = $('.images img.active');
+            var prossimaImg = $('.images img.active').prev();
+
+            imgAttiva.removeClass('active');
+            prossimaImg.addClass('active');
+
+            var pallinoAttivo = $('.slider-nav i.active');
+            var prossimaPallino = $('.slider-nav i.active').prev();
+
+            pallinoAttivo.removeClass('active');
+            prossimaPallino.addClass('active');
+        }
+    }
 
 
-var clock = setInterval(nextSlide,1000);
-    $('.images').mouseenter(function() {
-        clearInterval(clock);
-    });
-    $('.images').mouseleave(function() {
-        setTimeout(function() {
-            clock = setInterval(nextSlide,1000)
-            ;}, 1000);
-    });
+// var clock = setInterval(nextSlide,1000);
+//     $('.images').mouseenter(function() {
+//         clearInterval(clock);
+//     });
+//     $('.images').mouseleave(function() {
+//         setTimeout(function() {
+//             clock = setInterval(nextSlide,1000)
+//             ;}, 1000);
+//     });
 });
