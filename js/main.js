cbd44a3582e1d566f12e1ada0203b71880758825
Luca <luca-marconi@hotmail.com>
2020-03-02 14:53:02 +0100

    caricamento base esercizio

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $('.next').click(function() { // oppure anche cosi si puo scrivere  $('.next').click(nextImage);
+        nextSlide();
+    });
+
+    function nextSlide() {
+        if ($('.images img.active').hasClass('last')) {
+
+            $('.images img.active').removeClass('active');
+            $('.images img.first').addClass('active');
+
+            $('.slider-nav i.active').removeClass('active');
+            $('.slider-nav i.first').addClass('active');
+
+        } else {
+            var imgAttiva = $('.images img.active');
+            var prossimaImg = $('.images img.active').next();
+
+            imgAttiva.removeClass('active');
+            prossimaImg.addClass('active');
+
+            var pallinoAttivo = $('.slider-nav i.active');
+            var prossimaPallino = $('.slider-nav i.active').next();
+
+            pallinoAttivo.removeClass('active');
+            prossimaPallino.addClass('active');
+        }
+    }
+
+
+
+
+});
